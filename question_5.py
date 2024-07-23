2b2a0d7670f2f18d982778e0fae94bed04a0f44e
olintoeduardo <90293761+olintoeduardo@users.noreply.github.com>
2024-07-23 15:21:34 -0300

    Question 5 progress, implemented request in each of the CPI indexes from the csv file

--- question_5.py
+++ question_5.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
-
-
+#API request funcion
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/data/{data}")
+async def get_CPI_Data(data: str):
+    df = readCSV_into_df()
+    if data == "gasoline":
+        return get_gasoline_data(df)
+    elif data == "allItems":
+        return get_allItems_data(df)
+    elif data == "allItems-lessfood&energy":
+        return get_allItems_lessfoodenergy_data(df)
+
+#DataFrame operations
+def readCSV_into_df():
+    csvFile = "InflationUSA.csv"
+    df = pd.read_csv(csvFile,date_format=["Month"],index_col="Month")
+    return df
+
+def get_gasoline_data(dataframe):
+    gasoline = dataframe["Gasoline"]
+    gasoline.dropna(inplace = True)
+    return gasoline
+
+def get_allItems_data(dataframe: pd.DataFrame):
+    allItems = dataframe["All items"]
+    allItems.dropna(inplace = True)
+    return allItems
+
+def get_allItems_lessfoodenergy_data(dataframe: pd.DataFrame):
+    allItems_lessfoodenergy = dataframe["All items/less food&energy"]
+    allItems_lessfoodenergy.dropna(inplace = True)
+    return allItems_lessfoodenergy
